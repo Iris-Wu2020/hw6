6913f0ae1508de5f5bc8fedbfdcf5fc81daff6e6
IrisWu <xianwu_iris@126.com>
2021-03-02 21:26:41 +0800

    one change

--- js/ex6.js
+++ js/ex6.js
@@ -2,3 +2,40 @@
 Exercise 6 JavaScript code
 */
 console.log("Wu's Output from hw6 Exercise 6");
+
+const words = [{
+    term: "Procrastination",
+    definition: "Avoidance of doing a task that needs to be accomplished"
+  }, {
+    term: "Tautology",
+    definition: "logical argument constructed in such a way that it is logically irrefutable"
+  }, {
+    term: "Oxymoron",
+    definition: "figure of speech that juxtaposes elements that appear to be contradictory"
+  }];
+  console.log(words[0].term);
+
+
+//console.log(document.createElement("dt").textContent = "prodfdf");
+//   const dtNewNode2 = document.createElement("dt");
+//   const dtNewNode3 = document.createElement("dt");
+
+
+  
+  const buildNewNodeDt = (word) => {
+    const dtNewNode = document.createElement("dt");
+    dtNewNode.textContent = word;
+    return dtNewNode;
+  };
+
+  const buildNewNodeDd = (word) => {
+    const dtNewNode = document.createElement("dd");
+    dtNewNode.textContent = word;
+    return dtNewNode;
+  };
+
+  
+//console.log(buildNewNode("hhhhh"));
+console.log(words.map(word => buildNewNodeDt(word.term)));
+console.log(words.map(word => buildNewNodeDd(word.definition)));
+    
